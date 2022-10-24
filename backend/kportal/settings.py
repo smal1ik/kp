0af2fb5ac44a864a9fd087906facaf5d22db5d6b
MATVEI <motyha18@gmail.com>
2022-10-24 21:01:07 +0800

    add

--- backend/kportal/settings.py
+++ backend/kportal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x)0z7kxuckqm@=+cmw^sc3#cw7f6$!=*)5@*xtv4!kup#fuxet
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kptestv1.herokuapp.com']
 
 
 # Application definition
